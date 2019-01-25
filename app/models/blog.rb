7834dab86e62a6b9e9ac63eb171cdc1029619cba
shin <5446shin@gmail.com>
2019-01-25 12:58:40 +0000

    アソシエーションを実装

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,5 +2,6 @@ class Blog < ApplicationRecord
   validates :title, presence: true,length: { maximum: 50 }
   validates :content, presence: true,length: { maximum: 600 }
   mount_uploader :image, ImageUploader
+  belongs_to :user
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190125052538) do
+ActiveRecord::Schema.define(version: 20190125115605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20190125052538) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "image"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +34,5 @@ ActiveRecord::Schema.define(version: 20190125052538) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "blogs", "users"
 end
